e6c51a92b41320f3bdc981ab72f54bd7ccf6cf7d
AndrewJiao <646190632@qq.com>
2025-06-29 12:10:46 +0800

    feat:实现一个简单的读取yml功能

--- argument_parser/src/command_reader/mod.rs
+++ argument_parser/src/command_reader/mod.rs
@@ -1,24 +1,28 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
 ///
 /// 构造一个结构体用于承载我的所有Command配置
 /// 将来我将基于这些structs来生成命令行解析器
 ///
-pub struct AutoCommand<'a> {
-    name: &'a str,
-    about: &'a str,
-    subcommand_required: bool,
-    subcommand_required_else_help: bool,
-    subcommand: Option<&'a str>,
-    // args: Vec<clap::Arg<'a>>,
-    version: Option<&'a str>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AutoCommand {
+    name: String,
+    about: Option<String>,
+    subcommand_required: Option<bool>,
+    subcommand_required_else_help: Option<bool>,
+    subcommand: Option<String>,
+    args: Option<Vec<AutoArgument>>,
+    version: Option<String>,
 }
-
-pub struct AutoArgument<'a> {
-    name: &'a str,
-    required: bool,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AutoArgument {
+    name: String,
+    required: Option<bool>,
     num_args: Option<usize>,
-    default_value: Option<&'a str>,
-    default_missing_value: Option<&'a str>,
-    value_parser: Option<fn(&str) -> Result<String, String>>,
+    default_value: Option<String>,
+    default_missing_value: Option<String>,
 }
 
-
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AllCommand {}
--- entry_star/src/bin/argument_star.rs
+++ entry_star/src/bin/argument_star.rs
@@ -1,7 +1,20 @@
-use argument_parser::command_from_builder::do_command;
+use argument_parser::command_reader::AutoCommand;
+use std::collections::HashMap;
 
 fn main() {
     //读取配置
+    dotenv::dotenv().ok();
+    let yml_path = dotenv::var("BASIC_COMMAND_CONFIG")
+        .expect("no ARGUMENT_STAR_CONFIG set, please set it in .env file");
+    println!("reading yml config : {yml_path}");
+    //读取yml_path指向的文件
+    let pattern_model = std::fs::read_to_string(&yml_path).expect("Failed to read YAML file");
+    // Deserialize from YAML
+    let deserialized_point: HashMap<String,AutoCommand> = serde_yml::from_str(&pattern_model).expect("Failed to deserialize YAML");
+    println!("deserialized_point: {:#?}", deserialized_point);
+
+
+
     // do_test_arg()
-    do_command()
+    // do_command()
 }
